01ab5611cb88e45d488caf018b6c49f819f4eab4
Snowie Dev <lautarochini4@gmail.com>
2023-11-14 20:41:19 -0300

    export fix

--- app/(back)/api/auth/[...nextauth].js
+++ app/(back)/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
+// import clientPromise from "../../../lib/mongodb";
 
 export const authOptions = {
   // adapter: MongoDBAdapter(clientPromise),
