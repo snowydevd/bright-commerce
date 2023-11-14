eab2c8813598d6ab39aad050c8cf423ac568e360
Snowie Dev <lautarochini4@gmail.com>
2023-11-14 20:42:44 -0300

    export fix

--- app/(back)/api/auth/[...nextauth].js
+++ app/(back)/api/auth/[...nextauth].js
@@ -1,4 +1,3 @@
-import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 // import CredentialsProvider from "next-auth/providers/credentials";
 // import { MongoDBAdapter } from "@auth/mongodb-adapter";
--- app/(back)/api/auth/[...nextauth]/route.js
+++ app/(back)/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+import NextAuth from "next-auth";
 import { authOptions } from "../[...nextauth]";
 
 const handler = NextAuth(authOptions);
