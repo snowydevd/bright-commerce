8e3a7615ec99372b831aeb56c90c0510279ab6d8
Snowie Dev <lautarochini4@gmail.com>
2023-11-15 17:53:23 -0300

    db try

--- app/(back)/api/create-store-tables/route.js
+++ app/(back)/api/create-store-tables/route.js
@@ -5,8 +5,6 @@ export async function GET(request) {
   try {
     const result =
       await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    const names = ["Marcelo", "Juan"];
-    await sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
