ad4e665ac7740641314c0ebf5ef98b2f637e6b5a
Snowie Dev <lautarochini4@gmail.com>
2023-11-17 15:59:31 -0300

    db fix

--- app/(back)/api/create-store-tables/route.js
+++ app/(back)/api/create-store-tables/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   try {
     const result =
-      await sql`CREATE TABLE Products (id INT AUTO_INCREMENT PRIMARY KEY, stock INT, name varchar(255) NOT FULL, sizes varchar(255), photo varchar(255));`;
+      await sql`CREATE TABLE Products (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT FULL, sizes varchar(255), photo varchar(255));`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
